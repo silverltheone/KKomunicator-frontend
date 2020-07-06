edcef40b156c0aba3be82df836075076a4c3f34b
silverltheone <adeksiwy@gmail.com>
2020-07-06 19:34:43 +0200

    First commit

---
+++ src/main/java/com/comunicator/kkomunicatorfrontend/KkomunicatorFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.comunicator.kkomunicatorfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkomunicatorFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkomunicatorFrontendApplication.class, args);
+    }
+
+}
